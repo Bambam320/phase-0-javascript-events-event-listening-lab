f7a484d28a2499a8921af27ea67a492161310a5f
Igor Mestechkin <Igor.mestechkin@gmail.com>
2022-07-13 13:29:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+const input = document.getElementById('button')
+
+function addingEventListener() {    
+    input.addEventListener('click', () => alert('I was Clicked! and I am also a knucklehead'));
 }
+addingEventListener()
